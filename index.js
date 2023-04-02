4518112451e2e2ab3dcbf3496d12a312f80e6a5d
Pushpit <pushpit07@gmail.com>
2023-04-02 18:08:05 +0530

    Updated style
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const changeKey = () => {
 	document.getElementById("key_entered").style.display = "none";
 };
 
-document.getElementById("save_key_button").addEventListener("click", saveKey);
+// document.getElementById("save_key_button").addEventListener("click", saveKey);
 document.getElementById("change_key_button").addEventListener("click", changeKey);
 
 checkForKey().then((response) => {
